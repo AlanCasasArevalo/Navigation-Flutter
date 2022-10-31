5c0133d3de00b84a9ef0f7c364bfcd483fb62af7
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-31 07:40:44 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MenuPage(),
+    );
+  }
+}
+
+class MenuPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
