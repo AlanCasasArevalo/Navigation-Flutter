403b01d833b711c438807d24f6b8cdcab1f3854c
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-10-31 19:21:44 +0100

    Movemos la pagina de contador a su propio archivo

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/pages/menu_page.dart';
+import 'pages/counter_page.dart';
 import 'routes.dart';
 
 Map<String, Widget Function(BuildContext)> get appRoutes {
   return {
     '/': (context) => MenuPage(),
-    Routes.counter: (context) => Scaffold(
-          body: Center(
-            child: Text('Counter'),
-          ),
-        ),
+    Routes.counter: (context) => CounterPage()
   };
 }
