0b154abb9fb96c5dba2ddfd678cc1b6d5b3ca201
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-01 10:52:06 +0100

    Se muestra el comportamiento de WillPopScope

--- lib/pages/will_pop_scope_page.dart
+++ lib/pages/will_pop_scope_page.dart
@@ -3,10 +3,20 @@ import 'package:flutter/material.dart';
 class WillPopScopePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Counter'),
+    // Con este widget WillPopScope lo que hacemos "sobreescribir" el comportamiento del boton de retroceso nativo de android
+    // con el onWillPop lo que hacemos es devolver un valor FALSO si no queremos que se pueda usar el boton de regreso nativo,
+    // y un TRUE en caso de que si queramos permitir el retroceso con el boton nativo
+    return WillPopScope(
+      // Cuando se
+      onWillPop: () {
+        print("🤦🏻‍♂️");
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text('Will Pop Scope Page'),
+        ),
       ),
     );
   }
