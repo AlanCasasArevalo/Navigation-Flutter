1ea5972b273af76e828f5cffdba4cbaa89d76bce
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-01 09:57:32 +0100

    Otro tipo de redondeo

--- lib/dialogs/bottom_sheet_dialog.dart
+++ lib/dialogs/bottom_sheet_dialog.dart
@@ -2,45 +2,40 @@ import 'package:flutter/material.dart';
 
 Future<void> showBottomSheetDialog(BuildContext context) {
   return showModalBottomSheet(
-    backgroundColor: Colors.transparent,
-    context: context,
-    // Envolviendo el contenido en un Container podemos usar las propiedades de border radius debido a que el modal bottom no las tiene
-    builder: (context) => Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topRight: Radius.elliptical(20, 10),
-          topLeft: Radius.elliptical(20, 10),
-        ),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.elliptical(20, 10),
+        topRight: Radius.elliptical(20, 10),
       ),
-      child: SafeArea(
-        child: Column(
-          // Se hace que los elementos ocupen lo minimo posible
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              leading: Icon(Icons.photo),
-              title: Text("Photos"),
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              leading: Icon(Icons.south_rounded),
-              title: Text("Music"),
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              leading: Icon(Icons.video_camera_back_rounded),
-              title: Text("Video"),
-            ),
-          ],
-        ),
+    ),
+    context: context,
+    builder: (context) => SafeArea(
+      child: Column(
+        // Se hace que los elementos ocupen lo minimo posible
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            leading: Icon(Icons.photo),
+            title: Text("Photos"),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            leading: Icon(Icons.south_rounded),
+            title: Text("Music"),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            leading: Icon(Icons.video_camera_back_rounded),
+            title: Text("Video"),
+          ),
+        ],
       ),
     ),
   );
