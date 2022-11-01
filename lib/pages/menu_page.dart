2584cd2c2ad765000f22580f7d763ecff2817fb9
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-01 12:34:18 +0100

    Se agrega una funcion que sera la encargada de devolver un objeto cuando se requiera

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -12,21 +12,41 @@ class PageData {
   final String name;
   final String label;
   final Object? arguments;
+  final void Function(Object?)? onResult;
 
-  PageData({required this.name, required this.label, this.arguments});
+  PageData({
+    required this.name,
+    required this.label,
+    this.arguments,
+    this.onResult,
+  });
 }
 
 class _MenuPageState extends State<MenuPage> {
   Color _color = Colors.red;
 
-  final _pages = <PageData>[
-    PageData(
-        name: Routes.login, label: "Go to Login", arguments: 'test@test.com'),
-    PageData(name: Routes.counter, label: "Go to Counter"),
-    PageData(name: Routes.color_picker, label: "Go to Color picker"),
-    PageData(name: Routes.dialog, label: "Go to Color Dialog"),
-    PageData(name: Routes.will_pop_scope, label: "Go to Will Pop Scope"),
-  ];
+  late final List<PageData> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      PageData(
+          name: Routes.login, label: "Go to Login", arguments: 'test@test.com'),
+      PageData(name: Routes.counter, label: "Go to Counter"),
+      PageData(
+          name: Routes.color_picker,
+          label: "Go to Color picker",
+          onResult: (result) {
+            if (result != null && result is Color) {
+              _color = result;
+              setState(() {});
+            }
+          }),
+      PageData(name: Routes.dialog, label: "Go to Color Dialog"),
+      PageData(name: Routes.will_pop_scope, label: "Go to Will Pop Scope"),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,12 +60,15 @@ class _MenuPageState extends State<MenuPage> {
             final page = _pages[index];
             return ListTile(
               title: Text(page.label),
-              onTap: () {
-                Navigator.pushNamed(
+              onTap: () async {
+                final result = await Navigator.pushNamed(
                   context,
                   page.name,
-                  arguments: page.arguments
+                  arguments: page.arguments,
                 );
+                if (page.onResult != null) {
+                  page.onResult!(result);
+                }
               },
             );
           },
