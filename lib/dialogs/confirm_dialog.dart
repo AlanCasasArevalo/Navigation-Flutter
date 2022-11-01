51952ad38253bbec81ca76bdb521914a8ed1cc5e
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-01 09:18:20 +0100

    Customizacion del dialogo de confirmacion

--- lib/dialogs/confirm_dialog.dart
+++ lib/dialogs/confirm_dialog.dart
@@ -5,24 +5,67 @@ Future<bool> showConfirmDialog(
   String title = '',
 }) async {
   final result = await showDialog<bool>(
+    // Color de fondo del dialogo
+    barrierColor: Colors.white.withOpacity(.7),
     context: context,
-    builder: (context) => AlertDialog(
-      title: Text(title),
+    builder: (context) => _DialogContent(
+      title: title,
+    ),
+  );
+  return result ?? false;
+}
+
+class _DialogContent extends StatelessWidget {
+  final String title;
+
+  const _DialogContent({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      // Redondeo de los bordes, si no le pasamos borderRadius se quedaria rectangular completamente
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      // Color del dialogo
+      backgroundColor: Colors.blueGrey,
+      // Elevacion de la sombra
+      elevation: 10,
+      // Alineamiento del dialogo en toda la pantalla
+      alignment: Alignment.center,
+      // Alineamiento de los botones respecto al dialogo
+      actionsAlignment: MainAxisAlignment.center,
+      title: Text(
+        title,
+        textAlign: TextAlign.center,
+      ),
       actions: [
         MaterialButton(
           onPressed: () {
+            // Si no pasamos el segundo parametro a la vista padre (booleano) no se vera el print, debido a que es un dato null por defecto
             Navigator.pop(context, true);
           },
-          child: Text("Yes"),
+          child: Text(
+            "Yes",
+            style: TextStyle(
+              color: Colors.green,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ),
         MaterialButton(
           onPressed: () {
             Navigator.pop(context, false);
           },
-          child: Text("Nope"),
+          child: Text(
+            "Nope",
+            style: TextStyle(
+              color: Colors.red,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
         ),
       ],
-    ),
-  );
-  return result ?? false;
+    );
+  }
 }
